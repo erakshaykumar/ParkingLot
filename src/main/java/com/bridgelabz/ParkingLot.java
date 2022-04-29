83fecba4b23e3ffc636e59a97064bb1f9a91ee92
erakshaykumar <akaakshay304@gmail.com>
2022-04-29 18:33:11 +0530

    Added: Test And logic For UnPark A Vehicle In The Parking Lot

--- src/main/java/com/bridgelabz/ParkingLot.java
+++ src/main/java/com/bridgelabz/ParkingLot.java
@@ -1,12 +1,37 @@
+/**
+ * @purpose : Parking Lot System
+ * @author : Akshay kumar
+ */
 package com.bridgelabz;
 
 public class ParkingLot {
     Object vehicle = null;
 
+    /**
+     * UC1-Park Vehicle Logic
+     *
+     * @param: Vehicle
+     * @return: true/false
+     */
     public boolean parkVehicle(Object vehicle) {
         if (this.vehicle != null)
             return false;
         this.vehicle = vehicle;
         return true;
     }
+
+    /**
+     * UC2-UnPark Vehicle Logic
+     *
+     * @param: Vehicle
+     * @return: true/false
+     */
+    public boolean unParkVehicle(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -1,13 +1,70 @@
+/**
+ * @purpose : Parking Lot System
+ * @author : Akshay kumar
+ */
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotTest {
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+    Object vehicle2 = null;
+
+    @BeforeEach
+    public void setUp() {
+        parkingLot = new ParkingLot();
+        vehicle = new Object();
+        vehicle2 = new Object();
+    }
+
+    /**
+     * UC1-Vehicle When Parked Return True
+     *
+     * @param : Vehicle
+     */
+
     @Test
     public void givenVehicle_WhenPark_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        boolean parkingStatus = parkingLot.parkVehicle(new Object());
+        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
         Assertions.assertTrue(parkingStatus);
+    }
+
+    /**
+     * UC1-Vehicle When Already Parked Return False
+     *
+     * @param : Vehicle
+     */
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLot.parkVehicle(vehicle);
+        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
+        Assertions.assertFalse(parkingStatus);
+    }
+
+    /**
+     * UC2-Vehicle When UnParked Return True
+     *
+     * @param : Vehicle
+     */
+    @Test
+    public void givenVehicleIfParked_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.parkVehicle(vehicle);
+        boolean parkingStatus = parkingLot.unParkVehicle(vehicle);
+        Assertions.assertTrue(parkingStatus);
+    }
+
+    /**
+     * UC2-Vehicle When UnParked Return False
+     *
+     * @param : Vehicle
+     */
+    @Test
+    public void givenVehicleIfParked_AndGivenDifferentVehicleToUnPark_ShouldReturnFalse() {
+        parkingLot.parkVehicle(vehicle);
+        boolean parkingStatus = parkingLot.unParkVehicle(vehicle2);
+        Assertions.assertFalse(parkingStatus);
     }
 }
